56b9e9816bc28007054e7a189699f3026c38ba9b
daxmore <117888686+daxmore@users.noreply.github.com>
2022-11-10 11:02:04 +0530

    Web Desigh
    A Tailwindcss Project. Not Responsive.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      height: {
+        '128': '32rem',
+        '136': '46rem',
+        '140': '52em',
+        '138': '50em',
+      }
+    },
+  },
+  plugins: [],
+}
